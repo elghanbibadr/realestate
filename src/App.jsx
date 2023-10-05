3f31fad43286bd0674eac6afbe195aa4b94ddadd
badr ghanbi <bghanbi50@gmail.com>
2023-10-05 13:50:14 +0100

    create the background callout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./component/Navbar"
 import Hero from "./component/Hero"
 import { WhyUs } from "./component/WhyUs"
+import { WhatWeProvide } from "./component/WhatWeProvide"
+import { Callout } from "./component/Callout"
+
 const App = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
       <WhyUs />
+      <WhatWeProvide/>
+      <Callout/>
       </>
   )
 }
