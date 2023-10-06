3735a42d49de5fba269ecd63dee57b49ba02c459
badr ghanbi <bghanbi50@gmail.com>
2023-10-06 13:56:39 +0100

    instal daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//       colors:{
-//         darkBlue:"#161042",
-//         white:"#ffffff",
-//         cyan:'#0071F2',
-//         textColor:'#6E7681'
-//       }
-//   },
-//   plugins: [],
-// }
 
-// const withMT = require("@material-tailwind/react/utils/withMT");
- 
-// module.exports = withMT({
-//   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// });
 import withMT from "@material-tailwind/react/utils/withMT";
-// import { defineConfig } from 'vite-plugin-windicss';
-// import daisyUI from 'daisyui';
 
-// export default defineConfig({
-//   plugins: [daisyUI()],
-//   // ... other configurations
-// });
 
 export default withMT({
   content: ["./index.html", "./src/**/*.{js,jsx}"],
