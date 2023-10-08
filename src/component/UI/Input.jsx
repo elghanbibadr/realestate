81f6e64cb5170aff3e24212be2a7cc89249ef443
badr ghanbi <bghanbi50@gmail.com>
2023-10-08 17:54:25 +0100

    more work on the add property page

--- src/component/UI/Input.jsx
+++ src/component/UI/Input.jsx
@@ -4,7 +4,7 @@ export const Input = ({placeholder,type="text",className}) => {
   return (
 
     <>
-    <input type={type}  placeholder={placeholder} className={`input  relative outline-none  ${className}     border-white  bg-white  max-w-xs placeholder-darkBlue w-full  `} />
+    <input type={type}  placeholder={placeholder} className={`input  relative outline-none      border-white  bg-white  max-w-xs placeholder-darkBlue w-full ${className}  `} />
     {/* <img className="absolute right-2" src={pen} alt='pen icon' /> */}
     </>
     )
--- src/pages/AddYourProperty.jsx
+++ src/pages/AddYourProperty.jsx
@@ -2,8 +2,7 @@
 import Label from "../component/UI/Label"
 import Select from "../component/UI/Select"
 import { Input } from "../component/UI/Input"
-
-
+import map from "../assets/map1.png"
 export const AddYourProperty = () => {
   return (
     <div className="text-right  mt-10">
@@ -23,7 +22,7 @@ export const AddYourProperty = () => {
                   <span className="px-12 mx-4 border-x py-2  border-1 border-[#cccccc44] bg-white rounded-md ">  فيلا</span>
               </div>
 
-              <div className="mt-12 rightDir">
+              <div className="mt-12 rightDir ">
                   <h5 className="cyanTitle">الموقع</h5>
                   <div className="grid grid-cols-2 w-[60%]">
                       <div>
@@ -74,6 +73,22 @@ export const AddYourProperty = () => {
                     
                   </div>
                
+                <div>
+                 <div >
+                   <h5 className="cyanTitle mt-20">
+                   اكتب لنا مواصفات عقارك
+                     </h5>
+                        <Label className="dinNextLtRegular text-[22px] mt-0 " text="اكتب لنا مواصفات عقارك" />
+                        <Input placeholder="ادخل مواصفات دقيقة لعقارك " className="pb-20 pt-6 "  />
+                 </div>
+                 <div >
+                   <h5 className="cyanTitle mt-20 ">
+حدد موقع عقارك                     </h5>
+                        <Label className="dinNextLtRegular text-[22px] mt-0 " text="حدد على الخريطة لدقة اكبر" />
+                  <img src={map} alt="map image" />
+                  </div>
+
+                </div>
               </div>
            </div>
    </div>
