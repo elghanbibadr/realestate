029774596585056c36bb1ae05225a724e4e6c19d
badr ghanbi <bghanbi50@gmail.com>
2023-10-10 15:06:51 +0100

    created the navad page

--- src/pages/NavadPage.jsx
+++ src/pages/NavadPage.jsx
@@ -4,6 +4,7 @@ import Navad from "../assets/nafad.png"
 import globe from "../assets/globe.svg"
 import { Input } from '../component/UI/Input'
 import Label from '../component/UI/Label'
+import nic from "../assets/nic.png"
 import lock from "../assets/lock.svg"
 import appstorelogo from "../assets/apple-app-store-logo.jpg"
 import availableonappgallery from "../assets/availableonappgallery.png"
@@ -27,34 +28,60 @@ export const NavadPage = () => {
         </div>
 
         {/* BODY */}
-        <div className='mt-20'>
-            <h2 className='text-center'>الدخول على النظام</h2>
+        <div className='mt-20 '>
+            <h2 className='text-center dinNextLtRegular font-semibold text-darkGreen text-[30px]'>الدخول على النظام</h2>
             {/* navad surface card*/}
-            <div className='w-[80%] mx-auto '>
+            <div className='w-[80%] mx-auto  bg-[#F1F1F1] '>
                 <div className='bg-darkGreen rounded-md mt-6 p-4'>
-                    <h4 className='text-white text-[20px] text-center'>تطبيق نفاد</h4>
+                    <h4 className='text-white dinNextLtRegular font-semibold text-[20px] text-center'>تطبيق نفاد</h4>
                 </div>
-                <div className='w-[90%] grid grid-cols-2 mx-auto mt-10 p-6  bg-white rounded-md'>
+                <div className='w-[80%] grid grid-cols-2 mx-auto mt-10 p-6 pb-10 bg-white rounded-md'>
                 <div className=''>
                 <img className='h-40 mx-auto' src={lock} alt="lock icon" />
-                <p className='text-textColor text-center  text-[16px]'>    الرجاء ادخال رقم بطاقة الأحوال/الاقامة ثم    <br />اضغط دخول</p>
+                <p className='text-textColor dinNextLtRegular font-semibold text-center  text-[15px]'>    الرجاء ادخال رقم بطاقة الأحوال/الاقامة ثم    <br />اضغط دخول</p>
                 </div>
                 <div className='text-right'>
-                 {/* <Label className="text-textColor text-[16px]" text="رقم بطاقة الأحوال/الاقامة"/> */}
-                 <p className='text-textColor  text-[16px]'> رقم بطاقة الأحوال/الاقامة </p>
-                 <Input className="border-darkGreen my-3 text-right" placeholder="رقم بطاقة الأحوال/الاقامة " />
+                 <p className='text-textColor font-semibold  text-[15px]'> رقم بطاقة الأحوال/الاقامة </p>
+                 <input type="text" placeholder="ادخل رقم بطاقة الأحوال/الاقامة" className="input input-bordered input-accent border-darkGreen placeholder:opacity-30 w-full my-3 text-right mb-6" />
                  <button className="btn bg-darkGreen text-white w-full btn-accent"> تسجيل الدخول </button>
-                 <p className='text-textColor  text-[16px] text-center mt-6'>لتحميل تطبيق نفاد</p>
-                 <div className='grid grid-cols-3 gap-4  h-[10px]  mt-3'>
-                    <img className=' h-full  w-full' src={availableonappgallery} alt="app gallery logo" />
-                    <img className='h-full  w-full ' src={getitongoogleplay} alt="google play badge" />
-                    <img className='h-full  w-full ' src={appstorelogo} alt="app store badge" />
+                 <p className='text-textColor dinNextLtRegular font-semibold  text-[15px] text-center mt-6'>لتحميل تطبيق نفاد</p>
+                 <div className='grid grid-cols-3 gap-4   mt-4'>
+                    <img className=' h-[80%] object-fill  w-full' src={availableonappgallery} alt="app gallery logo" />
+                    <img className='h-[80%] object-fill  w-full ' src={getitongoogleplay} alt="google play badge" />
+                    <img className='h-[80%] object-fill  w-full ' src={appstorelogo} alt="app store badge" />
                  </div>
                 </div>
                 </div>
+          
+                <div className='bg-[#C2C2C2] rounded-md mt-6 p-4'>
+                    <h4 className='text-white dinNextLtRegular font-semibold text-[20px] text-center'> اسم المستخدم</h4>
+                </div>
             </div>
+        </div>
+
+
+        {/* footer */}
+        <div className='rightDir flex justify-between p-4  bg-[#F1F1F1] mt-20 py-8'>
+            <div className='flex'> 
+          <img className='h-12' src={nic} alt="nic image" />
+          <div>
+            <p className='text-black dinNextLtRegular font-semibold   text-[15px]'>تطوير وتشغيل</p>
+            <h3 className='dinNextLtRegular font-semibold text-[#5E6C79] text-[21px]'>مركز المعلومات الوطني</h3>
+             <p className='text-black dinNextLtRegular font-semibold   text-[15px]'>النفاد الوطني الموحد جميع الحقوق محفوظة  2023  </p> 
+
+          </div>
+            </div>
+            <ul className='flex items-center justify-between w-[40%] text-textColor dinNextLtRegular font-semibold text-center  text-[15px]'>
+             <li>الرئيسية</li>
+             <li> حول</li>
+             <li> اتصل بنا</li>
+             <li> الشروط والأحكام</li>
+             <li> المساعدة والدعم</li>
+             <li> سياسة الخصوصية</li>
+            </ul>
         </div>
 
     </div>
   )
 }
+
