96aa70917771ea660e983b7fc1883ae3d98a88a9
badr ghanbi <bghanbi50@gmail.com>
2023-10-11 11:40:32 +0100

    install needed dependency

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,50 +1,159 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { useState, useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-const containerStyle = {
-  width: '90%',
-  height: '90vh',
-  marginInline:"auto",
-  marginTop:"50px",
-};
+export default function Places() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
 
-const center = {
-  lat: 24.7136,
-  lng: 46.6753
-};
-
-function Map() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyC3Y-w5c-6fWUh5d-yLIm4iCBo_dGAeiqY"
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={14}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
 }
 
-export default Map
+function Map() {
+  const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
+  const [selected, setSelected] = useState(null);
+
+  return (
+    <>
+      <div className="places-container">
+        <PlacesAutocomplete setSelected={setSelected} />
+      </div>
+
+      <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        {selected && <Marker position={selected} />}
+      </GoogleMap>
+    </>
+  );
+}
+
+const PlacesAutocomplete = ({ setSelected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input"
+        placeholder="Search an address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+// const containerStyle = {
+//   width: '90%',
+//   height: '90vh',
+//   marginInline:"auto",
+//   marginTop:"50px",
+// };
+
+// const center = {
+//   lat: 24.7136,
+//   lng: 46.6753
+// };
+
+// function Map() {
+//   const { isLoaded } = useJsApiLoader({
+//     id: 'google-map-script',
+//     googleMapsApiKey: "AIzaSyC3Y-w5c-6fWUh5d-yLIm4iCBo_dGAeiqY"
+//   })
+
+//   const [map, setMap] = React.useState(null)
+
+//   const onLoad = React.useCallback(function callback(map) {
+//     // This is just an example of getting and using the map instance!!! don't just blindly copy!
+//     const bounds = new window.google.maps.LatLngBounds(center);
+//     map.fitBounds(bounds);
+
+//     setMap(map)
+//   }, [])
+
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
+
+//   return isLoaded ? (
+//       <GoogleMap
+//         mapContainerStyle={containerStyle}
+//         center={center}
+//         zoom={14}
+//         onLoad={onLoad}
+//         onUnmount={onUnmount}
+//       >
+//         { /* Child components, such as markers, info windows, etc. */ }
+//         <></>
+//       </GoogleMap>
+//   ) : <></>
+// }
+
+// export default Map
