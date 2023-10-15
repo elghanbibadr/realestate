f302996ea35c7847fdddfecd95d02f183c00b2a7
badr ghanbi <bghanbi50@gmail.com>
2023-10-15 20:57:40 +0100

    more fix

--- src/component/HeroCarousel.jsx
+++ src/component/HeroCarousel.jsx
@@ -7,43 +7,43 @@ export const HeroCarousel = () => {
         <img src={carouselImage1} className="w-full" />
         <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn bg-white border-white btn-circle">
-            <img className="rotate-90" src={arrow} />
+            <img className="rotate-90 h-5 w-5" src={arrow} />
           </a>
           <a href="#slide2" className="btn bg-white btn-circle border-white">
-            <img src={arrow} />
+            <img className="-rotate-90 h-5 w-5" src={arrow} />
           </a>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img src={carouselImage1} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn bg-white  btn-circle">
-            <img src={arrow} />
+          <a href="#slide1" className="btn bg-white  btn-circle border-white">
+            <img className="rotate-90 h-5 w-5" src={arrow} />
           </a>
-          <a href="#slide3" className="btn bg-white btn-circle">
-            ❯
+          <a href="#slide3" className="btn bg-white btn-circle border-white">
+            <img className="-rotate-90 h-5 w-5" src={arrow} />
           </a>
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img src={carouselImage1} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn bg-white btn-circle">
-            ❮
+          <a href="#slide2" className="btn bg-white btn-circle border-white">
+            <img className="rotate-90 h-5 w-5" src={arrow} />
           </a>
-          <a href="#slide4" className="btn bg-white btn-circle">
-            ❯
+          <a href="#slide4" className="btn bg-white btn-circle border-white">
+            <img className="-rotate-90 h-5 w-5" src={arrow} />
           </a>
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img src={carouselImage1} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn bg-white btn-circle">
-            ❮
+          <a href="#slide3" className="btn bg-white btn-circle border-white">
+            <img className="rotate-90 h-5 w-5" src={arrow} />
           </a>
-          <a href="#slide1" className="btn bg-white btn-circle">
-            ❯
+          <a href="#slide1" className="btn bg-white btn-circle border-white">
+            <img className="-rotate-90 h-5 w-5" src={arrow} />
           </a>
         </div>
       </div>
