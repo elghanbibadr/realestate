8d4517d93759bba096e3bd4bd4d996fcf25d438e
badr ghanbi <bghanbi50@gmail.com>
2023-10-18 15:42:01 +0100

    created the first row of the grid of the property features

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -3,6 +3,10 @@ import deg from "../assets/360deg.png";
 import gradient from "../assets/gradient.png";
 import Vector from "../assets/Vector (4).png";
 import descframe from "../assets/descframe.png";
+import playIcon from "../assets/playVector.png";
+import downloadIcon from "../assets/downloadArrow.png";
+import ellipse from "../assets/Ellipse.png";
+import success from "../assets/success.png";
 import badge from "../assets/badge.svg";
 import dottedgrp from "../assets/dottedGroup.png";
 import appartmentmajdia from "../assets/appartmentmajdia.png";
@@ -68,6 +72,82 @@ const PropertyDetails = () => {
         <button className="text-darkBlue bg-white  dinNextLtRegular  border-darkBlue border-[1px] px-16 py-2 mt-10 rounded-[4px] text-[20px] ">
           تحميل بروشور العقار
         </button>
+      </div>
+      {/* take tour */}
+      <div className="text-right mt-14">
+        <h5 className="text-darkBlue m-5 dinNextLtRegular text-[32px]">
+          خذ جولة بالعقار مع فريقنا{" "}
+        </h5>
+        <div className="bg-[#EDF2F7] h-[420px] text-center flex justify-center items-center mt-6">
+          <img src={playIcon} alt="play a video icon" />
+        </div>
+      </div>
+
+      {/* property features */}
+      <div className="text-right rightDir mt-20 p-10">
+        <h5 className="text-darkBlue  mb-6 dinNextLtRegular text-[32px]">
+          مميزات العقار
+        </h5>
+        <div className="grid grid-cols-3 gap-4 ">
+          {/* cols 1 */}
+          <div>
+            <div className="border-[1px] py-4 rounded-md pr-6 border-[#E2EDF9]">
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6> ترخيص البناء</h6>
+                <img
+                  className="mt-2 mx-2"
+                  src={downloadIcon}
+                  alt="download icon"
+                />
+              </div>
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6> شهادة اتمام البناء</h6>
+              </div>
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6> المخططات الانشائية</h6>
+                <img
+                  className="mt-2 mx-2"
+                  src={downloadIcon}
+                  alt="download icon"
+                />
+              </div>
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6> فايبر</h6>
+              </div>
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6> كهرباء</h6>
+              </div>
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6> صرف صحي</h6>
+              </div>
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6>تمديدات كميرات خارجية</h6>
+              </div>
+              <div className="flex items-center  text-darkBlue text-[18px] dinNextLtRegular font-semibold ">
+                <img className="mt-2" src={success} alt="blue checkmark icon" />
+                <h6> بدون التزامات على العقار</h6>
+              </div>
+            </div>
+            <div className="flex items-center gap-3 mt-6 border-[#E2EDF9] border-[1px] rounded-md pr-6 py-4">
+              <img src={ellipse} alt="ellipse image" />
+              <div className="dinNextLtRegular font-semibold">
+                <p className="text-darkBlue text-[16px]">
+                  هذا العقار من ضمن مشروع تطوير
+                </p>
+                <button className="bg-darkBlue text-[14px] mt-2 px-6 py-2 text-white rounded-md">
+                  عرض المشروع
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
