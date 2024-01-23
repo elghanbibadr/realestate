78261497e0fd5c75e516f8bb9e110bb4150cc904
Saif-Alotaibe <safe1otb@gmail.com>
2024-01-23 18:21:33 +0300

    Change Home component to class based component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import Hero from "../component/Hero";
-import { WhyUs } from "../component/WhyUs";
-import { WhatWeProvide } from "../component/WhatWeProvide";
-import { Background } from "../component/Background";
-import { ExploreOurProperties } from "../component/ExploreOurProperties";
-import Footer from "../component/Footer";
-import { Callout } from "../component/Callout";
+import WhyUs from "../component/WhyUs";
+import WhatWeProvide from "../component/WhatWeProvide";
+import Background from "../component/Background";
+import ExploreOurProperties from "../component/ExploreOurProperties";
+import { Component } from "react";
+import { connect } from "react-redux";
 
-export const Home = () => {
-  return (
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+class Home extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
     <>
       <main className="max-w-[1400px] mx-auto">
         <Hero />
@@ -18,4 +31,7 @@ export const Home = () => {
       </main>
     </>
   );
-};
+}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
