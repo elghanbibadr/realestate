fade431186ee7ff451f8fc1cb3044a524d3479b5
Saif-Alotaibe <safe1otb@gmail.com>
2024-01-23 18:01:41 +0300

    Change Background component to class based component

--- src/component/Background.jsx
+++ src/component/Background.jsx
@@ -1,12 +1,31 @@
+import { Component } from "react";
 import background from "../assets/Background.jpg";
-export const Background = () => {
-  return (
-    <div className="p-4 mt-10">
-      <img
-        className="w-full "
-        src={background}
-        alt="callout background image"
-      />
-    </div>
-  );
-};
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+class Background extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="p-4 mt-10">
+        <img
+          className="w-full "
+          src={background}
+          alt="callout background image"
+        />
+      </div>
+    );
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Background);
