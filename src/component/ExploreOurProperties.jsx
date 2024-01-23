bf4709ca332c98db7669c2e69a76ee2333de55f3
Saif-Alotaibe <safe1otb@gmail.com>
2024-01-23 18:03:18 +0300

    Change ExploreOurProperties to class based component

--- src/component/ExploreOurProperties.jsx
+++ src/component/ExploreOurProperties.jsx
@@ -1,20 +1,38 @@
+import { Component } from "react";
 import property1 from "../assets/property1.png";
 import property2 from "../assets/property2.png";
 import property3 from "../assets/property3.png";
+import { connect } from "react-redux";
 
-export const ExploreOurProperties = () => {
-  return (
-    <div className="rightDir p-4 mt-10">
-      <h5 className="text-cyan dinNextLtBold text-[24px]">اكتشف جديدنا</h5>
-      <h2 className=" dinNextLtBold text-[48px] text-darkBlue">
-        تصفح اجد عقاراتنا
-      </h2>
-      <div className="flex gap-6 mt-10">
-        <img src={property1} alt="property image and description" />
-        <img src={property2} alt="property image and description" />
-        <img src={property3} alt="property image and description" />
-        <img src={property1} alt="property image and description" />
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+class ExploreOurProperties extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="rightDir p-4 mt-10">
+        <h5 className="text-cyan dinNextLtBold text-[24px]">اكتشف جديدنا</h5>
+        <h2 className=" dinNextLtBold text-[48px] text-darkBlue">
+          تصفح اجد عقاراتنا
+        </h2>
+        <div className="flex gap-6 mt-10">
+          <img src={property1} alt="property image and description" />
+          <img src={property2} alt="property image and description" />
+          <img src={property3} alt="property image and description" />
+          <img src={property1} alt="property image and description" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ExploreOurProperties);
