03763e998cc5219cac8bf13ed60a9180befe3e07
Saif-Alotaibe <safe1otb@gmail.com>
2024-01-23 18:02:11 +0300

    Change Callout to class based component

--- src/component/Callout.jsx
+++ src/component/Callout.jsx
@@ -1,26 +1,46 @@
-export const Callout = () => {
-  return (
-    <div className="bg-white mt-40">
-      <div className="bg-cyan w-[70%] relative top-10 dinNextLtRegular  text-right flex justify-between mx-auto p-6 rounded-2xl">
-        <div className="flex">
-          <button className="btn bg-white hover:bg-white px-8 text-cyan">
-            اشترك الآن
-          </button>
-          <input
-            type="text"
-            placeholder="ادخل ايميلك"
-            className="input input-bordered w-full border-white max-w-xs placeholder-white  text-right relative top-1 mx-2"
-          />
-        </div>
-        <div>
-          <h4 className="text-white text-[24px]">
-            زودنا بإيميلك حتى يوصلك أحدث العقارات والعروض{" "}
-          </h4>
-          <p className="text-white text-[14px] opacity-80">
-            مع سوم عقار ما بيفوتك شي
-          </p>
+import { Component } from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+class Callout extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="bg-white mt-40">
+        <div className="bg-cyan w-[70%] relative top-10 dinNextLtRegular  text-right flex justify-between mx-auto p-6 rounded-2xl">
+          <div className="flex">
+            <button className="btn bg-white hover:bg-white px-8 text-cyan">
+              اشترك الآن
+            </button>
+            <input
+              type="text"
+              placeholder="ادخل ايميلك"
+              className="input input-bordered w-full border-white max-w-xs placeholder-white  text-right relative top-1 mx-2"
+            />
+          </div>
+          <div>
+            <h4 className="text-white text-[24px]">
+              زودنا بإيميلك حتى يوصلك أحدث العقارات والعروض{" "}
+            </h4>
+            <p className="text-white text-[14px] opacity-80">
+              مع سوم عقار ما بيفوتك شي
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Callout);
